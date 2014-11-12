4fe4591e1142716777d40143523b73c2df93d101
lwgray <lwgray@gmail.com>
2014-11-11 21:02:56 -0500

    Added environmental variable for twitter screen name

--- whatsmyipaddress.py
+++ whatsmyipaddress.py
@@ -9,10 +9,11 @@ import sys
 def check_in():
     ''' Check at 5am everyday or after reboot if dev server IP changed - if so,
     send me a twitter direct message'''
-    consumer_key = os.getenv('consumer_key')
-    consumer_secret = os.getenv('consumer_secret')
-    access_token_key = os.getenv('access_token_key')
-    access_token_secret = os.getenv('access_token_secret')
+    consumer_key = os.getenv('twitter_consumer_key')
+    consumer_secret = os.getenv('twitter_consumer_secret')
+    access_token_key = os.getenv('twitter_access_token_key')
+    access_token_secret = os.getenv('twitter_access_token_secret')
+    twitter_screen_name = os.getenv('twitter_screen_name')
     ip_address = open("current_ip_address.txt", 'r')
     for line in ip_address:
         address = line.strip()
@@ -26,7 +27,7 @@ def check_in():
                           consumer_secret=consumer_secret,
                           access_token_key=access_token_key,
                           access_token_secret=access_token_secret)
-        api.PostDirectMessage(screen_name='larrygray',
+        api.PostDirectMessage(screen_name=twitter_screen_name,
                               text='IP Address changed to {0}'.format(ext_ip))
         with open("current_ip_address.txt", 'w') as new_ip:
             new_ip.write(ext_ip)
