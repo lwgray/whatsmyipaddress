decbf32e730e036e4501160ae3f91b0511b4f751
lwgray <lwgray@gmail.com>
2016-01-27 16:12:31 -0500

    update dependencies

--- whatsmyipaddress.py
+++ whatsmyipaddress.py
@@ -1,5 +1,5 @@
+#! /usr/bin/env python
 '''Quick Script to monitor IP Address Changes'''
-# coding: utf-8
 import os
 import urllib2
 import twitter
@@ -14,19 +14,23 @@ def check_in():
     access_token_key = os.getenv('twitter_access_token_key')
     access_token_secret = os.getenv('twitter_access_token_secret')
     twitter_screen_name = os.getenv('twitter_screen_name')
-    ip_address = open("current_ip_address.txt", 'r')
-    for line in ip_address:
-        address = line.strip()
-    ip_address.close()
+    with open("current_ip_address.txt", 'r') as ip_address:
+        for line in ip_address:
+            address = line.strip()
     fqn = os.uname()[1]
+    # Reteive IPv6 address
     ext_ip = urllib2.urlopen('http://bot.whatismyipaddress.com').read().strip()
-    print ("Asset: %s " % fqn, "Checking in from IP#: %s " % ext_ip)
+    print("Asset: %s " % fqn, "Checking in from IP#: %s " % ext_ip)
+    # verify if IP address has changed
+    # if changed --> tweet direct message new address
+    # if not changed --> Do nothin
     match = address == ext_ip
     if not match:
-        api = twitter.Api(consumer_key=consumer_key,
-                          consumer_secret=consumer_secret,
-                          access_token_key=access_token_key,
-                          access_token_secret=access_token_secret)
+        print("IP Address has changed --> Tweet Sent!")
+        api = twitter.Api(consumer_key,
+                          consumer_secret,
+                          access_token_key,
+                          access_token_secret)
         api.PostDirectMessage(screen_name=twitter_screen_name,
                               text='IP Address changed to {0}'.format(ext_ip))
         with open("current_ip_address.txt", 'w') as new_ip:
